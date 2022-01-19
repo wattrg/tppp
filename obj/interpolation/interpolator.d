d9e61fdce238a35d5b502729bf4fd59be16f91a3
Robert Watt <wattrg@hotmail.com>
2022-01-19 13:30:14 +1000

    clean up object files

--- obj/interpolation/interpolator.d
+++
@@ -1,4 +0,0 @@
-obj/interpolation/interpolator.o: src/interpolation/interpolator.cpp \
- src/interpolation/interpolator.h
-src/interpolation/interpolator.cpp:
-src/interpolation/interpolator.h:
--- obj/interpolation/linear_interpolator.d
+++
@@ -1,5 +0,0 @@
-obj/interpolation/linear_interpolator.o: src/interpolation/linear_interpolator.cpp \
- src/interpolation/linear_interpolator.h src/interpolation/interpolator.h
-src/interpolation/linear_interpolator.cpp:
-src/interpolation/linear_interpolator.h:
-src/interpolation/interpolator.h:
